19c492e3f97696a187419af8ec7905d9c5b4b5ba
Crsando <qiu.mingda.pku@outlook.com>
2024-07-21 11:59:22 +0800

    update

--- test_md.lua
+++ test_md.lua
@@ -11,11 +11,27 @@ collector:start()
 
 print("---")
 
+local i = 0
+
+local accum_time = 0
+
 while true do 
     local tick = collector:recv()
+
+    local t0 = os.clock()
     tick = ctp.totable(tick)
+    local t1 = os.clock()
+    accum_time = accum_time + (t1 - t0)
+    print(accum_time)
 
-    print(inspect(tick))
+    -- print(inspect(tick))
 
+    i = i + 1
+
+    if i > 100 then 
+        break 
+    end
     -- print(ffi.string(tick.ActionDay), tick.LastPrice)
 end
+
+print("avg", accum_time / i)
